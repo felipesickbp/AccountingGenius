cf9187acf40e2a6f6683ebf3631218a4d6507484
felipesickbp <sick@burkhart-partners.ch>
2025-06-17 16:11:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ TEMPLATE_ORDER = [
     "Soll", "Haben", "MWST Code", "MWST Konto",
 ]
 
-_MWST_ACCOUNTS = {
+MWST_ACCOUNTS = {
     "1500", "1510", "1520", "1530",        # Maschinen, Mobiliar, IT, Fahrzeuge
     "5008",                                # übriger Personalaufwand
     "5810", "5820", "5821", "5880",        # Weiterbildung / Spesen / Anlässe
